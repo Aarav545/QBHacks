a546cc65de6cc3f4d01d8233c726607aba3136e7
Ansh2 <43502767+Ansh2@users.noreply.github.com>
2023-03-28 22:37:49 -0400

    Update HomePage.js

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -33,11 +33,11 @@ export default class HomePage extends React.Component {
                   </Grid.Row>
 
                   <Grid.Row>
-                    <Header className={"date"}>December 1, 2019</Header>
+                    <Header className={"date"}>March 18, 2023</Header>
                   </Grid.Row>
                   <Grid.Row>
                     <div className={"reg-button-contain"}>
-                      <Button target={"blank"} href={"https://rohandeshpande725226.typeform.com/to/M4XEyz"}className={"reg-button"}>Register</Button>
+                      <Button target={"blank"} href={"https://forms.gle/MbxJGD3WkwRG6Xxy9"}className={"reg-button"}>Register</Button>
                     </div>
                   </Grid.Row>
                 </div>
